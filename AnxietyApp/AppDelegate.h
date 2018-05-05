9365dba560f1312a4846dd34058e049bce237f51
Div Bhasin <divbhasin@v1020-wn-115-57.campus-dynamic.uwaterloo.ca>
2018-05-05 09:27:26 -0400

    Initial Commit

---
+++ AnxietyApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnxietyApp
+//
+//  Created by Div Bhasin on 2018-05-05.
+//  Copyright © 2018 Div Bhasin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnxietyApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnxietyApp
+//
+//  Created by Div Bhasin on 2018-05-05.
+//  Copyright © 2018 Div Bhasin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
