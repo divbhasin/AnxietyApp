9da6c1b69cd262fcc434adb9aa037670363e46ac
Div Bhasin <divbhasin@v1020-wn-115-57.campus-dynamic.uwaterloo.ca>
2018-05-05 10:48:25 -0400

    Added configuration code

--- AnxietyApp/AppDelegate.m
+++ AnxietyApp/AppDelegate.m
@@ -12,11 +12,14 @@
 
 @end
 
+@import Firebase;
+
 @implementation AppDelegate
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [FIRApp configure];
     return YES;
 }
 
